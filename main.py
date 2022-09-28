a2eab34c1d64cc2480f21895da1756cb537c8656
Timo Vilen <timo.a.vilen@tuni.fi>
2022-09-28 10:47:12 +0300

    Timing the user input.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import curses
+from time import perf_counter
 # Show text
 # Allow user to input text
 # Check if input_char is equal to string[X] 
 # Count mistakes
 # Add possibility to amend your input
-# name = ""
+# Calculate the words per minute
 
 def check_for_diffences(input_string: str, message: str) -> int:
     errors = 0
@@ -26,6 +27,12 @@ def show_text(stdscr, middle_row: int, message: str, num_cols: int) -> int:
 
     return x_position
 
+def words_per_minute():
+    # The value is calculated with the following formula:
+    # Every character of the string divided by 5 per minutes minus uncorrected errors per minutes
+    #  
+    pass
+
 def get_input(stdscr, middle_row: int, cursor_pos: int, message: str):
 
     user_input = ""
@@ -34,10 +41,14 @@ def get_input(stdscr, middle_row: int, cursor_pos: int, message: str):
     user_editing = False
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     stdscr.keypad(True)
-
+    # start = None
     # Main loop
+    
     while True:
         input_char = stdscr.get_wch()
+        if len(user_input) == 0:
+            # Start time
+            time_start = perf_counter()
         if input_char == curses.KEY_LEFT:
             # If the user presses the left arrow key and there are char in the list:
             # Back the index down by one and make the cursor visible to the user.
@@ -57,13 +68,15 @@ def get_input(stdscr, middle_row: int, cursor_pos: int, message: str):
 
         if len(user_input) == len(message):
             break
+    
+    time_end = perf_counter()
 
     count = check_for_diffences(user_input, message)
     if count > 0:
-        error_message = f"Out of {len(user_input)} characters, you have {count} errors"
+        error_message = f"Out of {len(user_input)} characters, you have {count} errors."
         stdscr.addstr(middle_row + 3, cursor_pos, error_message)
 
-    stdscr.addstr(middle_row + 6, cursor_pos, "Done")
+    stdscr.addstr(middle_row + 6, cursor_pos, f"Done - It took you {time_end - time_start} seconds")
     stdscr.getkey()
 
 def main(stdscr):
